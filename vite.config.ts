f25a5e1558021c1d45c22fd8c73e3cee57c9c8dd
gabrieljm37Br <gabrieljm37@gmail.com>
2025-11-25 22:08:27 -0300

    refactor: update Vite alias resolution to use `new URL` and `import.meta.url`

--- vite.config.ts
+++ vite.config.ts
@@ -1,4 +1,3 @@
-import path from 'path';
 import { defineConfig, loadEnv } from 'vite';
 import react from '@vitejs/plugin-react';
 
@@ -18,7 +17,7 @@ export default defineConfig(({ mode }) => {
     },
     resolve: {
       alias: {
-        '@': path.resolve(__dirname, '.'),
+        '@': new URL('.', import.meta.url).pathname,
       }
     }
   };
